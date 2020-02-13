de8e6af25e68a93a7e0000a43b821d5b4595c03d
Timka8888 <tim.stankevich@gmail.com>
2020-02-14 00:38:36 +0200

    add section products

--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,8 @@ gulp.task('script', function(){
 
 gulp.task('js', function(){
   return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js'
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/mixitup/dist/mixitup.js'
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
